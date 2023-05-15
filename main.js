b8b42437a8eefdda06299177b53a1aeb9adb5d73
Fombi-Favour <fombifavour@gmail.com>
2023-05-15 11:15:29 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 const hambuger = document.querySelector('.menu');
 const navMenu = document.querySelector('.nav-menu');
 
-function
+function showMenu() {
+    navMenu.style.right = "-90px";
+}
+
+function hideMenu() {
+    navMenu.style.right = "-800px";
+}
