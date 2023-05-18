8a73cca9f755772b6752306aba002e88ff91b6c1
Fombi-Favour <fombifavour@gmail.com>
2023-05-18 23:02:41 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const hambger = document.querySelector('#nav-menus');
 const menu = document.querySelector('#menu-icon');
 const closex = document.querySelector('.menu');
 const list = document.querySelectorAll('.items');
-const mode = document.querySelector('.all');
 
 hambger.addEventListener('click', () => {
   hambger.classList.add('hide');
@@ -19,73 +18,4 @@ Array.from(list).forEach((item)=>{
     hambger.classList.remove('hide');
     menu.classList.remove('show');
   }); 
-})
-// popup window
- const popup = [
-  {
-    id: 1,
-    title: 'Tonic',
-    Organization:'Canopy',
-    Stack: ['Back End','2015'],
-    image: 'images/first.png',
-    Discription: 'loremipsum',
-    Technologies:['HTML' ,'CSS' , 'JavaScript'],
-    seeLive:'btn',
-    seeSource:'button'
-
-  }
-  ,
-  {
-    id: 2,
-    title: 'Tonic',
-    Organization:'Canopy',
-    Stack: ['Back End','2015'],
-    image: 'images/first.png',
-    Discription: 'loremipsum',
-    Technologies:['HTML' ,'CSS' , 'JavaScript'],
-    seeLive:'btn',
-    seeSource:'button'
-
-  }
-  ,
-  {
-    id: 3,
-    title: 'Tonic',
-    Organization:'Canopy',
-    Stack: ['Back End','2015'],
-    image: 'images/first.png',
-    Discription: 'loremipsum',
-    Technologies:['HTML' ,'CSS' , 'JavaScript'],
-    seeLive:'btn',
-    seeSource:'button'
-
-  }
-  ,
-  {
-    id: 4,
-    title: 'Tonic',
-    Organization:'Canopy',
-    Stack: ['Back End','2015'],
-    image: 'images/first.png',
-    Discription: 'loremipsum',
-    Technologies:['HTML' ,'CSS' , 'JavaScript'],
-    seeLive:'btn',
-    seeSource:'button'
-
-  }
-]
-
-mode.addEventListener('click', (e) => {
-    const container=document.querySelector('.work-section');
-    popup.map((project)=>{
-     
-      const card=document.createElement('div');
-      card.classList.add('card-section');
-      const image=document.createElement('img');
-      image.classList.add('profile');
-      image.src=project.image;
-      card.appendChild(image);
-      
-      container.appendChild(card);
-    })
-  });
+});
